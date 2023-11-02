ca708aef934e3a9694f5bcfa94bbdd43e81cef9f
Cedar Drake <cedar.drake@mrjaffesclass.com>
2023-11-02 11:42:55 -0700

    final

--- ScannerReadCSVFile-master/ScannerReadCSVFile.java
+++ ScannerReadCSVFile-master/ScannerReadCSVFile.java
@@ -24,43 +24,22 @@ public class ScannerReadCSVFile
             // Read the next line of the file
             String line = scanner.nextLine();
 
-            // line now contains a line of comma-separated numbers
-            // representing 10 test scores for each class.
-            //
-            // Your job is to parse the numbers into individual test scores using
-            // another Scanner variable using a comma as the delimiter.  
-            // 
-            // Read the ten test scores and average them together.  The first
-            // number is the class number and the next 10 numbers are the 
-            // test scores.  
-            //
-            // You need to compute the average and print for each class the class
-            // number, then the average score.  The first few lines should look
-            // like this:
-            //
-            // Class: Avg score
-            // 2125:  55
-            // 1628:  47
-            //
-            // Pay attention to the spacing on your output
-            //
-            // Write your code in the space below!
             if (i != 1) {
                 Scanner scan = new Scanner(line).useDelimiter(",|\n");
-                int g = 0;
-                int c = 0;
-                int a = 0;
-                boolean sc = false;
+                int grade = 0;
+                int Class = 0;
+                int average = 0;
+                boolean SetClass = false;
                 while (scan.hasNextInt()) {
-                    if (sc == false) {
-                        c = scan.nextInt();
-                        sc = true;
+                    if (SetClass == false) {
+                        Class = scan.nextInt();
+                        SetClass = true;
                     } else {
-                        a += scan.nextInt();
-                        g++;
+                        average += scan.nextInt();
+                        grade++;
                     }
                 }
-                System.out.println(c + ": " + a/g);
+                System.out.println(Class + ": " + average / grade);
             }
             i++;
             
